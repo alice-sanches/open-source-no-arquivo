c64b5529810f1d8dc9ad97970dae1ecc94eee063
alice-sanches <alicensanches27@gmail.com>
2024-04-06 18:37:47 +0100

    data fetched into json

---
+++ page_data_fetch.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+termos_pesquisa = ["código aberto", "open source", "software livre", "acesso aberto", "linux", "unix"]
+urls = []
+
+def make_urls():
+    for termo in termos_pesquisa:
+        urls.append(f"https://arquivo.pt/textsearch?q=\"{termo}\"&maxItems=50&prettyPrint=true")
+    return(urls)
+
+make_urls()
+print(urls)
+
+with open('page_data.json', 'w') as f:
+    for url in urls:
+        print(url)
+        response = requests.get(url)
+        print(response) 
+        json.dump(response.json(), f)
+        f.write('\n')
+    
+ 
