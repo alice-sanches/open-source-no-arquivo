66e58371ef257714bed88fca7dacc774b9734cf6
alice-sanches <alicensanches27@gmail.com>
2024-05-25 14:27:31 +0100

    write seed and instance data to csv

--- second-collection/diacronic_info_fetch.py
+++ second-collection/diacronic_info_fetch.py
@@ -1,14 +1,28 @@
 import json
 import pandas as pd
-import re
-# from selected_urls import selected_urls
+import csv
 
 capture_count = []
+selected_urls = []
 
-with open('D:\Projetos\Programming\open-source-no-arquivo\second-collection\selected_urls_metadata.json', 'r') as metadata:
-    json_data = json.load(metadata)
-    for link in metadata:
-        capture_count.append('estimated_nr_results')
+with open('D:\Projetos\Programming\open-source-no-arquivo\second-collection\selected_urls_metadata.json', 'r') as f:
+    json_data = json.load(f)
+    for link in json_data:
+        capture_count.append(link['estimated_nr_results'])
 
 print(capture_count)
+
+with open ('D:\Projetos\Programming\open-source-no-arquivo\second-collection\selected_urls.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in reader:
+            selected_urls.append(row[0])
+
+seed_data = pd.DataFrame(
+    {
+       "seed_url": selected_urls,
+       "number_of_results": capture_count
+     }
+)
+
+seed_data.to_csv(r'D:\Projetos\Programming\open-source-no-arquivo\second-collection\seed_data.csv')
 
---
+++ second-collection/site_instance_info_fetch.py
@@ -0,0 +1,31 @@
+import json
+import pandas as pd
+import csv
+
+original_urls = []
+timestamps = []
+collections = []
+titles = []
+content_lengths = []
+
+with open('D:\Projetos\Programming\open-source-no-arquivo\second-collection\selected_urls_metadata.json', 'r') as f:
+    json_data = json.load(f)
+    for seed in json_data:
+        for site_instance in seed["response_items"]:
+            original_urls.append(site_instance["originalURL"])
+            timestamps.append(site_instance["tstamp"])
+            collections.append(site_instance["collection"])
+            titles.append(site_instance["title"])
+            content_lengths.append(site_instance["contentLength"])
+        
+site_instance_data = pd.DataFrame(
+    {
+       "seed_url": original_urls,
+       "timestamp": timestamps,
+       "collection": collections,
+       "title": titles,
+       "contentLength" : content_lengths
+     }
+)
+
+site_instance_data.to_csv(r'D:\Projetos\Programming\open-source-no-arquivo\second-collection\site_instance_data.csv')
